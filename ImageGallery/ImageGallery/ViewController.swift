8a9172cbc48f43cc2898e220130b4cd96866c2b4
Erick Silva <ericksilva1988@gmail.com>
2019-09-30 15:20:16 -0300

    Adding Project

---
+++ ImageGallery/ImageGallery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageGallery
+//
+//  Created by Erick Silva on 9/30/19.
+//  Copyright © 2019 ericksilva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
